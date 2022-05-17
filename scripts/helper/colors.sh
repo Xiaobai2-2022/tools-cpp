8e2487ad952e7a29dc70704fe3ac3f03fa26c580
Zhifan Li <z2367li@uwaterloo.ca>
2022-05-17 19:46:29 -0400

    Added Scripts

---
+++ scripts/helper/colors.sh
@@ -0,0 +1,9 @@
+# auth: University of Waterloo Department of Software Engineering
+# changed by: Zhifan (Xiaobai2)
+#!/bin/bash
+
+# defining colors
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+CYAN="\033[0;36m"
+NC='\033[0m' # No Color
